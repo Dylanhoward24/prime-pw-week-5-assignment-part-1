107e85200e5233c75073469e00e7077eecc69a10
Dylan Howard <dylanhoward0424@outlook.com>
2021-07-11 12:24:50 -0500

    added nested arrays for favorite foods

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -32,8 +32,8 @@ console.log( fullName );
   - Console.log your first favorite food
   - Console.log your last favorite food
 */
-
-
+console.log( 'my first favorite food is: ' + me.favThreeFoods[0]);
+console.log( 'my last favorite food is: ' + me.favThreeFoods[me.favThreeFoods.length - 1]);
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
